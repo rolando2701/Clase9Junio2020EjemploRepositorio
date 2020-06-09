39afaf516ff32e9c5d692307e599f395d5ce011f
rolando2701 <rolando2701@hotmail.com>
2020-06-09 13:32:58 -0600

    Agregué código en la línea 21 del metodo Main

--- Clase9Junio2020_RepositorioGithub/src/clase9junio2020_repositoriogithub/Clase9Junio2020_RepositorioGithub.java
+++ Clase9Junio2020_RepositorioGithub/src/clase9junio2020_repositoriogithub/Clase9Junio2020_RepositorioGithub.java
@@ -17,6 +17,9 @@ public class Clase9Junio2020_RepositorioGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        
+        System.out.println("Saludo a Yareth y Cristian");
+        
     }
     
 }
