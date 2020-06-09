d31f7c7c3f12886caa5d0a970aca93fc6504e09b
rolando2701 <rolando2701@hotmail.com>
2020-06-09 13:27:50 -0600

    primera carga del proyecto

---
+++ Clase9Junio2020_RepositorioGithub/src/clase9junio2020_repositoriogithub/Clase9Junio2020_RepositorioGithub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase9junio2020_repositoriogithub;
+
+/**
+ *
+ * @author rolan
+ */
+public class Clase9Junio2020_RepositorioGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
